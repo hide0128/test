f4d08f9b2b577288d8a131720f47e8be12d3e33a
hide0128 <hide@jbcc.co.jp>
2025-08-06 13:31:16 +0900

    フィボナッチ関数と素数判定関数を追加

---
+++ test.py
@@ -0,0 +1,21 @@
+def fibonacci(n):
+    # ここにフィボナッチ数列を計算するコードを書いてください。
+    # Claudeに手伝ってもらおう！
+    if n <= 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fibonacci(n - 1) + fibonacci(n - 2)
+    # 5番目のフィボナッチ数を計算して表示してみよう
+result = fibonacci(5)
+print(result)
+def is_prime(num):
+    if num < 2:
+        return False
+    for i in range(2, int(num ** 0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
+
+print(f"Is {result} prime? {is_prime(result)}")
